3c53854b0130c4612240445201c31b52482c1d73
Lizzz7 <lizethtitovilca@gmail.com>
2025-07-18 13:36:36 -0500

    Usar \n antes de cada imagen

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ENCYCLOPEDIA_DATA = {
         "content": (
             "ARCHI es un asistente virtual diseñado para ayudarte a explorar temas relacionados con sistemas digitales y analógicos, micro-operaciones, circuitos lógicos y más. "
             "Utiliza el menú principal para navegar por los temas o utiliza la función de búsqueda para encontrar información específica."
-            "[IMAGE:micro.png]"
+            "\n[IMAGE:micro.png]"
             "[IMAGE:micro.png] Más texto."
         ),
     },
